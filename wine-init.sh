2fe8b2c6ea3113afb1628ef6403db1bdfb9bd357
Tobias Gruetzmacher <tobias-git@23.gs>
2024-02-28 21:31:57 +0100

    Remove mention of setting the default to Windows 10
    Windows 10 is the default for new prefixes since Wine 9.0 (see the Wine
    9.0 release notes:
    https://gitlab.winehq.org/wine/wine/-/releases/wine-9.0)

--- wine-init.sh
+++ wine-init.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # Prepare environment:
-# - Set Windows version to 10 (disabled for now)
 # - Disable menu updates
 # - Disable Mono
 # - Disable Gecko
@@ -8,7 +7,6 @@ set -e
 
 umask 0
 export WINEDLLOVERRIDES="winemenubuilder.exe,mscoree,mshtml="
-#winecfg -v win10
 wine reg add 'HKCU\Software\Wine\DllOverrides' /v winemenubuilder.exe /t REG_SZ /d '' /f
 wine reg add 'HKCU\Software\Wine\DllOverrides' /v mscoree /t REG_SZ /d '' /f
 wine reg add 'HKCU\Software\Wine\DllOverrides' /v mshtml /t REG_SZ /d '' /f
